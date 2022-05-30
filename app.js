d932dc907c3916e1126aa74334f94af1b09c6c05
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-05-30 16:43:15 +0200

    add fonctionality of mooving and adding numbers

--- app.js
+++ app.js
@@ -13,17 +13,20 @@ addEventListener("DOMContentLoaded", () => {
 
       gridDisplay.appendChild(square);
       squares.push(square);
+
+      //   squares.forEach((element) => {
+      //     gridDisplay.appendChild(element);
+      //   });
     }
     generate();
     generate();
   }
 
   createBoard();
+
   //generate a numer two randomly
   function generate() {
     let randomNumber = Math.floor(Math.random() * squares.length);
-
-
 
     if (squares[randomNumber].innerHTML == 0) {
       squares[randomNumber].innerHTML = 2;
@@ -32,27 +35,178 @@ addEventListener("DOMContentLoaded", () => {
 
   //swipe right
 
-//   function moveRight() {
+  function moveRight() {
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML;
+        let totalTwo = squares[i + 1].innerHTML;
+        let totalThree = squares[i + 2].innerHTML;
+        let totalFour = squares[i + 3].innerHTML;
 
-//     for (let i = 0; i < 16; i++) {
-//         console.log(i);
-//       if (i % 4 === 0) {
-//         let totalOne = squares[i].innerHTML;
-//         let totalTwo = squares[i + 1].innerHTML;
-//         let totalThree = squares[i + 2].innerHTML;
-//         let totalFour = squares[i + 3].innerHTML;
+        let row = [
+          parseInt(totalOne),
+          parseInt(totalTwo),
+          parseInt(totalThree),
+          parseInt(totalFour),
+        ];
 
-//         let row = [
-//           parseInt(totalOne),
-//           parseInt(totalTwo),
-//           parseInt(totalThree),
-//           parseInt(totalFour),
-//         ];
+        let filteredRow = row.filter((num) => num);
+        let missing = 4 - filteredRow.length;
+        console.log(missing);
+        let zeros = Array(missing).fill(0);
 
-//         console.log(row);
-//       }
-//     }
-//   }
+        let newRow = zeros.concat(filteredRow);
 
-//   moveRight();
+        squares[i].innerHTML = newRow[0];
+        squares[i + 1].innerHTML = newRow[1];
+        squares[i + 2].innerHTML = newRow[2];
+        squares[i + 3].innerHTML = newRow[3];
+      }
+    }
+  }
+
+  //swipe left
+  function moveLeft() {
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML;
+        let totalTwo = squares[i + 1].innerHTML;
+        let totalThree = squares[i + 2].innerHTML;
+        let totalFour = squares[i + 3].innerHTML;
+
+        let row = [
+          parseInt(totalOne),
+          parseInt(totalTwo),
+          parseInt(totalThree),
+          parseInt(totalFour),
+        ];
+
+        let filteredRow = row.filter((num) => num);
+        let missing = 4 - filteredRow.length;
+
+        let zeros = Array(missing).fill(0);
+
+        let newRow = filteredRow.concat(zeros);
+
+        squares[i].innerHTML = newRow[0];
+        squares[i + 1].innerHTML = newRow[1];
+        squares[i + 2].innerHTML = newRow[2];
+        squares[i + 3].innerHTML = newRow[3];
+      }
+    }
+  }
+
+  function moveDown() {
+    for (let i = 0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML;
+      let totalTwo = squares[i + width].innerHTML;
+      let totalThree = squares[i + width * 2].innerHTML;
+      let totalFour = squares[i + width * 3].innerHTML;
+
+      let column = [
+        parseInt(totalOne),
+        parseInt(totalTwo),
+        parseInt(totalThree),
+        parseInt(totalFour),
+      ];
+
+      let filteredColumn = column.filter((num) => num);
+      let missing = 4 - filteredColumn.length;
+
+      let zeros = Array(missing).fill(0);
+      let newColumn = zeros.concat(filteredColumn);
+
+      squares[i].innerHTML = newColumn[0];
+      squares[i + width].innerHTML = newColumn[1];
+      squares[i + width * 2].innerHTML = newColumn[2];
+      squares[i + width * 3].innerHTML = newColumn[3];
+    }
+  }
+  function moveUp() {
+    for (let i = 0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML;
+      let totalTwo = squares[i + width].innerHTML;
+      let totalThree = squares[i + width * 2].innerHTML;
+      let totalFour = squares[i + width * 3].innerHTML;
+
+      let column = [
+        parseInt(totalOne),
+        parseInt(totalTwo),
+        parseInt(totalThree),
+        parseInt(totalFour),
+      ];
+
+      let filteredColumn = column.filter((num) => num);
+      let missing = 4 - filteredColumn.length;
+
+      let zeros = Array(missing).fill(0);
+      let newColumn = filteredColumn.concat(zeros);
+
+      squares[i].innerHTML = newColumn[0];
+      squares[i + width].innerHTML = newColumn[1];
+      squares[i + width * 2].innerHTML = newColumn[2];
+      squares[i + width * 3].innerHTML = newColumn[3];
+    }
+  }
+
+  function combineRow() {
+    for (let i = 0; i < 15; i++) {
+      if (squares[i].innerHTML == squares[i + 1].innerHTML) {
+        let combineTotal =
+          parseInt(squares[i].innerHTML) + parseInt(squares[i + 1].innerHTML);
+        squares[i].innerHTML = combineTotal;
+        squares[i + 1].innerHTML = 0;
+      }
+    }
+  }
+
+  function combineColumn() {
+    for (let i = 0; i < 12; i++) {
+      if (squares[i].innerHTML == squares[i + width].innerHTML) {
+        let combineTotal =
+          parseInt(squares[i].innerHTML) +
+          parseInt(squares[i + width].innerHTML);
+        squares[i].innerHTML = combineTotal;
+        squares[i + width].innerHTML = 0;
+      }
+    }
+  }
+
+  function control(e) {
+    if (e.keyCode === 39) {
+      keyRight();
+    } else if (e.keyCode === 37) {
+      keyLeft();
+    } else if (e.keyCode === 38) {
+      keyUp();
+    } else if (e.keyCode === 40) {
+      keyDown();
+    }
+  }
+  document.addEventListener("keyup", control);
+
+  function keyRight() {
+    moveRight();
+    combineRow();
+    moveRight();
+    generate();
+  }
+  function keyLeft() {
+    moveLeft();
+    combineRow();
+    moveLeft();
+    generate();
+  }
+  function keyDown() {
+    moveDown();
+    combineColumn();
+    moveDown();
+    generate();
+  }
+  function keyUp() {
+    moveUp();
+    combineColumn();
+    moveUp();
+    generate();
+  }
 });
